ed37e7d7a181a333fcd98c98e088c60e6305fd58
Andrey Gurevich <andrey.gurevich@tabby.ai>
2023-12-10 18:48:47 +0200

    v 0.0.3 - Some GCP improvements

--- app/config.py
+++ app/config.py
@@ -1,5 +1,7 @@
-# Telegram bot token
-BOT_TOKEN = ""
-API_KEY=""
-IDS_ALLOWED=[]
+from os import getenv
 
+TELEGRAM_BOT_TOKEN = getenv("TELEGRAM_BOT_TOKEN")
+RAINDROP_API_TOKEN = getenv("RAINDROP_API_TOKEN")
+RAINDROP_API_ENDPOINT = "https://api.raindrop.io/rest/v1/raindrop"
+BOT_ENDPOINT = getenv("BOT_ENDPOINT") # Must be in format "https://your-cloud-run-service-url/"
+# IDS_ALLOWED=[]
--- app/main.py
+++ app/main.py
@@ -2,42 +2,23 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import re
 import requests
-from os import getenv
+from config import RAINDROP_API_ENDPOINT, BOT_ENDPOINT, RAINDROP_API_TOKEN, TELEGRAM_BOT_TOKEN
 
-# Define your bot token and Raindrop.io API token here
-TELEGRAM_BOT_TOKEN = getenv("TELEGRAM_BOT_TOKEN")
-RAINDROP_API_TOKEN = getenv("RAINDROP_API_TOKEN")
-
-
-# Raindrop.io API endpoint
-RAINDROP_API_ENDPOINT = "https://api.raindrop.io/rest/v1/raindrop"
-
-
-# Function to handle the /start command
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Hello! I am your URL parser bot. Send me a message, and I will find and save the URLs for you.")
 
-
-# Function to handle incoming messages
 def parse_url(update: Update, context: CallbackContext) -> None:
-    # Get the text of the received message
     message_text = update.message.text
-
-    # Use regular expression to find URLs in the message
     urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message_text)
 
     if urls:
-        # Save each URL to Raindrop.io
         for url in urls:
             save_url_to_raindrop(url)
 
-        # Send a confirmation message
         update.message.reply_text(f"Found and saved the following URLs to your Raindrop.io account:\n{', '.join(urls)}")
     else:
         update.message.reply_text("No URLs found in the message.")
 
-
-# Function to save a URL to Raindrop.io
 def save_url_to_raindrop(url: str) -> None:
     headers = {
         "Authorization": f"Bearer {RAINDROP_API_TOKEN}",
@@ -46,7 +27,7 @@ def save_url_to_raindrop(url: str) -> None:
 
     data = {
         "link": url,
-        "collection": 0,  # Change this to the collection ID where you want to save the links
+        "collection": 0,
     }
 
     response = requests.post(RAINDROP_API_ENDPOINT, headers=headers, json=data)
@@ -56,26 +37,23 @@ def save_url_to_raindrop(url: str) -> None:
     else:
         print(f"Failed to save URL '{url}' to Raindrop.io. Status code: {response.status_code}, Response: {response.json()}")
 
+def health_check(update, context):
+    update.message.reply_text("I'm healthy!")
 
-# Main function to run the bot
 def main() -> None:
     updater = Updater(TELEGRAM_BOT_TOKEN)
-
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Register command handlers
     dp.add_handler(CommandHandler("start", start))
-
-    # Register a message handler to parse URLs
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, parse_url))
+    dp.add_handler(CommandHandler("health", health_check))
 
-    # Start the Bot
-    updater.start_polling()
+    # Start the Bot with webhook
+    updater.start_webhook(port=8080, listen="0.0.0.0", url_path=TELEGRAM_BOT_TOKEN)
+    updater.bot.set_webhook(BOT_ENDPOINT + TELEGRAM_BOT_TOKEN)
 
     # Run the bot until you send a signal to stop it
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
